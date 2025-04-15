95e4a34f7417d844488a8c909af9a905175b74cb
Mohamed-Ashraf186 <mohamedashraf28812@gmail.com>
2025-04-15 17:32:40 +0200

    optimized routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Appointments from "../pages/Appointments/Appointments";
 import Patients from "../pages/Patients/Patients";
@@ -6,16 +11,16 @@ import Layout from "../components/Layout";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="appointments" element={<Appointments />} />
-          <Route path="patients" element={<Patients />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/patients" element={<Patients />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
